5b54c55654a4a42b6e3886f0942ddf05e5a738c2
omardhoor <omardhoore80@gmail.com>
2026-01-12 22:15:07 +0300

    error fixed commit

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
 
+// Laravel on Vercel needs to use /tmp for storage
+$viewPath = '/tmp/views';
+if (!is_dir($viewPath)) {
+    mkdir($viewPath, 0777, true);
+}
+
+// Override the environment variables for Vercel if not set
+putenv("VIEW_COMPILED_PATH=$viewPath");
+
 // Forward Vercel requests to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
